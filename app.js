5c558b05f753b8f7f89c158d3dea22f75a8be45e
ashramen <ashrahman18@gmail.com>
2021-10-05 17:53:41 -0400

    db connection

--- app.js
+++ app.js
@@ -1,2 +1,7 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
+
+mongoose
+  .connect(process.env.MONGODB_SRV)
+  .then(() => console.log('Connected to database'))
+  .catch((err) => console.log(err));
