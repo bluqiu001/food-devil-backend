f2a6f009ab7d44b2e0c3d77ccc6749ac1a398012
ashramen <ashrahman18@gmail.com>
2021-10-05 18:13:40 -0400

    example DB

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
 const Cat = require('./models/catSchema');
+//^Imports
 
+//Connect to mongo atlas
 mongoose
   .connect(process.env.MONGODB_SRV)
   .then(() => console.log('Connected to database'))
   .catch((err) => console.log(err));
 
-const kitty = new Cat({ name: 'Peter' });
+//Create a new cat object called kitty
+const kitty = new Cat({ name: 'Peter', color: 'Blue' });
+
+//Save and upload this object to the collection of cats then print meow
 kitty.save().then(() => console.log('meow'));
