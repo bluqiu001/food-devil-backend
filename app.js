c13e93157d0bcc98b3c9b5066116f42e8aa3a527
ashramen <ashrahman18@gmail.com>
2021-10-05 18:05:58 -0400

    test DB connection

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
+const Cat = require('./models/catSchema');
 
 mongoose
   .connect(process.env.MONGODB_SRV)
   .then(() => console.log('Connected to database'))
   .catch((err) => console.log(err));
+
+const kitty = new Cat({ name: 'Peter' });
+kitty.save().then(() => console.log('meow'));
