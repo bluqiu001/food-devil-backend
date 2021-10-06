e819401de3e9dc5a23be39b7db439dc37dd3b39e
Jerry Lin <jerrylin3321@gmail.com>
2021-10-06 18:22:58 -0400

    added user to app.js

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
 const Cat = require('./models/catSchema');
+const User = require('./models/userSchema');
+
 //^Imports
 
 //Connect to mongo atlas
@@ -11,6 +13,9 @@ mongoose
 
 //Create a new cat object called kitty
 const kitty = new Cat({ name: 'Peter', color: 'Blue' });
+const user = new User({first_name: 'Jerry', last_name: 'Lin', username: 'jerrylin'})
 
 //Save and upload this object to the collection of cats then print meow
 kitty.save().then(() => console.log('meow'));
+user.save().then(() => console.log('meow'));
+
