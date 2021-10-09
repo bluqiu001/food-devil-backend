366581076a5cc7f3eb535bbdda2b90ff079d2645
ashramen <ashrahman18@gmail.com>
2021-10-09 16:28:53 -0400

    clean up app

--- db/conn.js
+++ db/conn.js
@@ -1,5 +1,5 @@
-const { MongoClient } = require("mongodb");
-//const Db = process.env.ATLAS_URI;
+const { MongoClient } = require('mongodb');
+
 const Db = process.env.MONGODB_SRV;
 const client = new MongoClient(Db, {
   useNewUrlParser: true,
@@ -12,16 +12,15 @@ module.exports = {
   connectToServer: function (callback) {
     client.connect(function (err, db) {
       // Verify we got a good "db" object
-      if (db)
-      {
-        _db = db.db("myFirstDatabase");
-        console.log("Successfully connected to MongoDB."); 
+      if (db) {
+        _db = db.db('myFirstDatabase');
+        console.log('Successfully connected to MongoDB.');
       }
       return callback(err);
-         });
+    });
   },
 
   getDb: function () {
     return _db;
   },
-}; 
+};
