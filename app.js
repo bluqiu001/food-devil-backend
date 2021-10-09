30756d6e1aa5bf0c7a3dd6e9ea817e9bdff31115
Jerry Lin <jerrylin3321@gmail.com>
2021-10-09 16:59:40 -0400

    added mealSchema, and added meal route template code

--- app.js
+++ app.js
@@ -4,16 +4,25 @@ const Restaurant = require('./models/restaurantSchema');
 const Food = require('./models/foodSchema');
 const Review = require('./models/reviewSchema');
 const User = require('./models/userSchema');
+const Meal = require('./models/mealSchema');
 
 mongoose
   .connect(keys.mongodb_srv)
   .then(() => console.log('Connected to database'))
   .catch((err) => console.log(err));
 
-const userTest = new User({
+/**const userTest = new User({
   full_name: 'Ashabur Rahman',
   username: 'Test123-345',
   password: 'xyza',
 });
 
 userTest.save().then(() => console.log('Saved user'));
+**/
+
+const mealTest = new Meal({
+  user_id: 'testtest',
+  foods: ['615e25ee7a51ba6ed79c9b02', '615e25ee7a51ba6ed79c9b02']
+});
+
+mealTest.save().then(() => console.log('Saved user'));
