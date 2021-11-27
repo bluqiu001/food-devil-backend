4834e0fe1c8fea063389e2ff80e317efdab5b57c
ashramen <ashrahman18@gmail.com>
2021-11-27 17:34:24 -0500

    add automatic restaurant entry for foods

--- api/routes/meals.js
+++ api/routes/meals.js
@@ -1,70 +1,73 @@
-const express = require('express');
-const router = express.Router();
-const Meal = require('../../models/mealSchema');
-const checkAuth = require('../middleware/check-auth');
-
-// router.get('/', checkAuth, (req, res) => {
-//   Meal.find()
-//     .exec()
-//     .then((docs) => {
-//       res.status(200).json(docs);
-//     })
-//     .catch((err) => {
-//       res.status(500).json({ error: err });
-//     });
-// });
+const express = require('express')
+const router = express.Router()
+const Meal = require('../../models/mealSchema')
+const checkAuth = require('../middleware/check-auth')
+const Food = require('../../models/foodSchema')
 
 router.get('/getUserMeals/:mealId', checkAuth, (req, res) => {
-  const id = req.params.mealId;
-  Meal.find({user_id: id})
+  const id = req.params.mealId
+  Meal.find({ user_id: id })
     .exec()
     .then((doc) => {
       if (doc) {
-        res.status(200).json(doc);
+        res.status(200).json(doc)
       } else {
-        res.status(404).json({ message: 'No meal found for provided ID' });
+        res.status(404).json({ message: 'No meal found for provided ID' })
       }
     })
     .catch((err) => {
-      res.status(500).json({ error: err });
-    });
-});
+      res.status(500).json({ error: err })
+    })
+})
 
 router.get('/:mealId', checkAuth, (req, res) => {
-  const id = req.params.mealId;
+  const id = req.params.mealId
   Meal.findById(id)
     .exec()
     .then((doc) => {
       if (doc) {
-        res.status(200).json(doc);
+        res.status(200).json(doc)
       } else {
-        res.status(404).json({ message: 'No meal found for provided ID' });
+        res.status(404).json({ message: 'No meal found for provided ID' })
       }
     })
     .catch((err) => {
-      res.status(500).json({ error: err });
-    });
-});
+      res.status(500).json({ error: err })
+    })
+})
 
-router.post('/', checkAuth, (req, res, next) => {
-  //const id = req.params.mealId;
+router.post('/', checkAuth, async (req, res, next) => {
+  var restaurants = []
+  await Food.find({
+    _id: { $in: req.body.foods },
+  })
+    .exec()
+    .then((doc) => {
+      for (const entry of doc) {
+        restaurants.push(entry.restaurantId)
+      }
+    })
+    .catch((err) => console.log(err))
+
   const meal = new Meal({
     user_id: req.body.user_id,
-    foods: req.body.foods
-  });
+    foods: req.body.foods,
+    restaurants: restaurants,
+  })
+
   meal
     .save()
     .then((doc) => {
       res.status(201).json({
         message: 'Meal created',
         createdReview: doc,
-      });
+      })
     })
-    .catch((err) => res.status(500).json(err));
-});
+    .catch((err) => res.status(500).json(err))
+})
 
 router.patch('/:mealId', checkAuth, (req, res, next) => {
-  const id = req.params.mealId;
+  const id = req.params.mealId
   Meal.updateOne(
     { _id: id },
     {
@@ -75,11 +78,11 @@ router.patch('/:mealId', checkAuth, (req, res, next) => {
   )
     .exec()
     .then((doc) => res.status(200).json(doc))
-    .catch((err) => res.status(500).json(err));
-});
+    .catch((err) => res.status(500).json(err))
+})
 
 router.delete('/:mealId', checkAuth, (req, res, next) => {
-  const id = req.params.mealId;
+  const id = req.params.mealId
   Meal.remove({ _id: id })
     .exec()
     .then((doc) => res.status(200).json(doc))
@@ -87,7 +90,7 @@ router.delete('/:mealId', checkAuth, (req, res, next) => {
       res.status(500).json({
         error: err,
       }),
-    );
-});
+    )
+})
 
-module.exports = router;
+module.exports = router
--- models/mealSchema.js
+++ models/mealSchema.js
@@ -1,17 +1,17 @@
-const mongoose = require('mongoose');
-const ObjectId = require('mongodb').ObjectId;
-const Schema = mongoose.Schema;
+const mongoose = require('mongoose')
+const ObjectId = require('mongodb').ObjectId
+const Schema = mongoose.Schema
 
 const mealSchema = new mongoose.Schema(
   {
     user_id: { type: String, required: true },
-    foods: [{ type: Schema.Types.ObjectId, ref: 'Food' }]
-
+    foods: [{ type: Schema.Types.ObjectId, ref: 'Food' }],
+    restaurants: [{ type: Schema.Types.ObjectId, ref: 'Restaurant' }],
   },
   {
     timestamps: true,
   },
-);
-const Meal = mongoose.model('Meal', mealSchema);
+)
+const Meal = mongoose.model('Meal', mealSchema)
 
-module.exports = Meal;
+module.exports = Meal
